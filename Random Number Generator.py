ac99d4ed35d2d499fd08f18dedfd0aed69f34199
SH Sifat <shamirulhassansifat@gmail.com>
2024-03-03 08:36:53 +0600

    Created Random Number Generator APP

---
+++ Random Number Generator.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from random import randint
+
+app =Tk()
+app.title("Intro")
+app.geometry("500x300")
+Heading = Label(app, text="Generate a Random Number")
+Heading.pack()
+
+def show_msg():
+    msg = Label(app, text=randint(1,200))
+    msg.pack()
+
+b = Button(app,text ="Generate", command=show_msg)
+b.pack()
+
+app.mainloop()
+
