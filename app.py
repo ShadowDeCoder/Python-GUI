af95d75d2bbd36610d245ab3c91328beec707eb0
SH Sifat <shamirulhassansifat@gmail.com>
2024-03-09 12:30:57 +0600

    Theme Edited

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ from random import choice
 app = Tk()
 
 app.title("Sample")
-app.geometry("500x300")
+app.geometry("220x150")
+app['background'] = 'gold'
+
 Heading = Label(app, text="Letes Code", font=("Courier", 12))
 Heading.grid()
 
-entry = Entry(app)
+entry = Entry(app, background='grey')
 entry.grid(row=2, column=0, columnspan=2, padx=20, pady=6)
 
 
 def show():
     show =entry.get().split(",")
     text = 'Choice : '+choice(show)
-    msg = Label(app, text=text, font=("Courier", 12))
-    msg.grid(row=4, column=0,columnspan=2, padx=20, pady=6)
+    msg = Label(app, text=text, font=("Courier", 12), background='pink', foreground='white', relief="raised")
+    msg.grid(row=4, column=0, columnspan=2, padx=20, pady=6)
 
     if quit["state"] == DISABLED:
         quit["state"] = NORMAL
@@ -24,10 +26,10 @@ def show():
 
 
 b = Button(app, text="Enter",command=show)
-b.grid(row=3, column=0, padx=20, pady=6)
+b.grid(row=3, column=0, padx=20, pady=6, )
 
 
-quit = Button(app, text=" Close ", command=quit, state= DISABLED)
+quit = Button(app, text=" Close ", command=quit, state=DISABLED, background='red', foreground='white')
 quit.grid(row=3, column=1, padx=20, pady=6)
 
 
